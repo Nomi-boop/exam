f1b8d7cbf992717b85e13d8c4bbad63a4515ab3a
unknown <nominadmit@gmail.com>
2022-10-04 15:51:51 +0300

    created string arrays

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] arr1 = {"Hello", "2", "world", ":-)"};
+string[] arr2 = {"1234", "1567", "-2", "computer science"};
+string[] arr3 = {"Russia", "Denmark", "Kazan"};
